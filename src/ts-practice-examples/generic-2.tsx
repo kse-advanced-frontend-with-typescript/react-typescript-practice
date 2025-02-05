929eb104193f4b78837eee8d327041f2061c3eb7
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2025-02-05 21:18:21 +0200

    add examples

--- src/ts-practice-examples/generic-2.tsx
+++ src/ts-practice-examples/generic-2.tsx
@@ -2,17 +2,29 @@ import React, {useEffect, useState} from "react";
 
 export const List: React.FC = () => {
     const [result, setResult] = useState(false);
+    const [time, setTime] = useState<string>(new Date().toUTCString());
     useEffect(() => {
-        setResult(calculate())
-    })
+        setTimeout(() => {
+            setTime(new Date().toUTCString())
+        }, 1000)
+        const res = calculate()
+        setResult(res as unknown as boolean)
+    }, [time])
 
-    return <div>Calculated: {result ? 'done' : 'in progress'}</div>
+    return <div>
+        <p>{time}</p>
+        <p>Calculated: {result ? 'done' : 'in progress'}</p>
+    </div>
 }
 
 
 function calculate() {
-    for (let i = 0; i < 1999999999; i++) {
+    for (let i = 0; i < 3999999999; i++) {
         //
     }
     return true;
+}
+
+function memo(fn: (...args: unknown[]) => unknown) {
+    // to implement!
 }
